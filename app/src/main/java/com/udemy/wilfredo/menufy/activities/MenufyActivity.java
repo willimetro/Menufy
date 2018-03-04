1d683da57e3944493606aa6d1a5a67686f582017
willimetro <wilfredo.ugalde@gmail.com>
2018-03-04 01:03:42 -0300

    Initial commit

---
+++ app/src/main/java/com/udemy/wilfredo/menufy/activities/MenufyActivity.java
@@ -0,0 +1,15 @@
+package com.udemy.wilfredo.menufy.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.udemy.wilfredo.menufy.R;
+
+public class MenufyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menufy);
+    }
+}
